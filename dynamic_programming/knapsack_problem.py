19f6638ef8cd7b20835ca1e60539a60fcd276cc4
Kaba Yusuf <y_kabs@yahoo.com>
2018-03-13 17:53:50 +0100

    -dynamic programming: Knapsack Problem - use only previous solution

--- dynamic_programming/knapsack_problem.py
+++ dynamic_programming/knapsack_problem.py
@@ -6,19 +6,22 @@ def knapsack(file_path) -> int:
     value_list, weight_list, capacity = convert_file_to_knapsack(file_path)
     solution = []
 
-    # add rows:
-    for x in range(len(value_list)):
-        solution.append([])
+    # add two columns to only remember previous solution:
+    for x in range(2):
+        solution.append({})
 
     # initialize first column to zero
     for x in range(capacity + 1):
-        solution[0].append(0)
+        solution[0][x] = 0
 
     for i in range(1, len(value_list)):
         for x in range(capacity + 1):
             if x < weight_list[i]:
-                solution[i].append(solution[i - 1][x])
+                solution[1][x] = solution[0][x]
             else:
-                solution[i].append(max(solution[i - 1][x], (solution[i - 1][x - weight_list[i]] + value_list[i])))
+                solution[1][x] = (max(solution[0][x], (solution[0][x - weight_list[i]] + value_list[i])))
 
-    return solution[len(value_list) - 1][capacity]
+        solution[0] = solution[1]
+        solution[1] = {}
+
+    return solution[0][capacity]
